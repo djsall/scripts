f304b268f04a59c6a72a9058d6b1cd073c5c9a31
DjSall <proksalevente@gmail.com>
2018-06-03 14:37:17 +0200

    Added FAQ section

--- update-namecheap/update-namecheap.sh
+++ update-namecheap/update-namecheap.sh
@@ -2,9 +2,12 @@
 # script for updating Namecheap's dynamic DNS service.
 # Dependencies: curl 
 # Author: DjSall
+#
+#FAQ:
+#Running it in Git Bash on Windows:
+# Change EOL encoding to LF and run it like this "sh update-namecheap.sh"
 ################################################################################
 
-unset username password ip
 update_url='dynamicdns.park-your-domain.com/update'
 username=''
 password=''
@@ -24,7 +27,6 @@ if [[ $ip ]];
   else
     ip="the ip the request was made from."
 fi
-echo "$url"
 
 response=$(curl "${url}" 2> /dev/null)
 
